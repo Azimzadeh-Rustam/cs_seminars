243524196eeddca99e0503532fdc0326467d5cf4
Rustam <azimzadeh.rustam@gmail.com>
2023-08-02 14:52:22 +0400

    modified Ex002_HW

--- Ex002_HW/Program.cs
+++ Ex002_HW/Program.cs
@@ -3,7 +3,7 @@
 Console.Write("Введите первое число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе число число: ");
+Console.Write("Введите второе число: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 
 if (numberA > numberB) {
