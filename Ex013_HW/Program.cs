4da2a834176a54f5141b0a6d368d83c7e53e483f
Rustam <azimzadeh.rustam@gmail.com>
2023-08-09 19:01:19 +0400

    Add Ex013_HW

---
+++ Ex013_HW/Program.cs
@@ -0,0 +1,36 @@
+﻿// 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int ShowThirdDigit(int num)
+{
+    while(num > 999)
+    {
+        num /= 10;
+    }
+    return num % 10;
+}
+
+bool Validate(int num)
+{
+    if(num < 100)
+    {
+        Console.WriteLine("Третьей цифры нет.");
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+int number = Prompt("Введите число: ");
+if(Validate(number))
+{
+    Console.WriteLine(ShowThirdDigit(number));
+}
