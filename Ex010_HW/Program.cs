e816e0b80f65cde283c3b6003f3ebeba49825c91
Rustam <azimzadeh.rustam@gmail.com>
2023-08-09 18:16:48 +0400

    Add Ex010_HW

---
+++ Ex010_HW/Program.cs
@@ -0,0 +1,14 @@
+﻿// 10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.s
+
+int ShowSecondDigit(int num)
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+    int secondDigit = (num - firstDigit * 100 - thirdDigit) / 10;
+    return secondDigit;
+}
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(ShowSecondDigit(number));
