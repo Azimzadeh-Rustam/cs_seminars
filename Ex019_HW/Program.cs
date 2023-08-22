41e809deefd11205ceb1b9196fe248206fc2ef31
Rustam <azimzadeh.rustam@gmail.com>
2023-08-22 23:47:27 +0400

    Add Ex_019_HW

---
+++ Ex019_HW/Program.cs
@@ -0,0 +1,18 @@
+﻿// 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+bool Validation (int number)
+{
+    return number > 9999 && number < 100000 ? true : false;
+}
+
+void IsPalindrome(int number)
+{
+    if (number / 10000 == number % 10 && (number / 1000) % 10 == (number / 10) % 10) Console.WriteLine("Это палиндром");
+    else Console.WriteLine("Это не палиндром");
+}
+
+Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (Validation(n)) IsPalindrome(n);
+else Console.WriteLine("Число не пятизначное");
