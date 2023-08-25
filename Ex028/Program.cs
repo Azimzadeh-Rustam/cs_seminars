cb7e78382e64a6699ed6f6827e04ab1057881fb7
Rustam <azimzadeh.rustam@gmail.com>
2023-08-25 22:28:17 +0400

    Add Ex028

---
+++ Ex028/Program.cs
@@ -0,0 +1,26 @@
+﻿// 28. Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+int MultiplicationNumbers(int A)
+{
+    int sum = 1;
+    int i = 1;
+    while(i <= A)
+    {
+        // Проверка на переполнение типа (Int32)
+        checked
+        {
+            sum *= i;
+            i++;
+        }
+    }
+    return sum;
+}
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+while(number < 1)
+{
+    Console.Write("Введите число больше единицы: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"Произведение от 1 до {number} равна {MultiplicationNumbers(number)}");
