c704bf21ab432a7b14ddc4a117e56f130466db56
Rustam <azimzadeh.rustam@gmail.com>
2023-08-30 18:37:14 +0400

    Add Ex029_HW

---
+++ Ex029_HW/Program.cs
@@ -0,0 +1,37 @@
+﻿// 29. Напишите программу, которая задаёт массив из 8 элементов, заполненный псевдослучайными числами и выводит их на экран.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string readInput = Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+int length = Prompt("Укажите длину массива: ");
+int min = Prompt("Укажите нижний диапазон для значений элементов массива: ");
+int max = Prompt("Укажите верхний диапазон для значений элементов массива: ");
+PrintArray(CreateArrayRndInt(length, min, max));
