3f211b0be261060f268e8240741349399b5577d9
Rustam <azimzadeh.rustam@gmail.com>
2023-09-05 23:18:03 +0400

    Add Ex046

---
+++ Ex046/Program.cs
@@ -0,0 +1,34 @@
+﻿// 46. Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] arr)
+{
+    int rows = arr.GetLength(0);
+    int columns = arr.GetLength(1);
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            Console.Write($"{arr[i, j], 5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrixRndInt(5, 5, 0, 10);
+PrintArray(matrix);
