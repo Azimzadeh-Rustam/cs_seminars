f68fcfbf5e4410b79844cf1599d7daf153a5396a
Rustam <azimzadeh.rustam@gmail.com>
2023-09-05 23:18:48 +0400

    Ex049

---
+++ Ex049/Program.cs
@@ -0,0 +1,52 @@
+﻿// 49. Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+int[,] SquareBothEvenIndexes(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+
+    for(int i = 0; i < rows; i += 2)
+    {
+        for(int j = 0; j < columns; j += 2)
+        {
+            matrix[i, j] *= matrix[i, j];
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            Console.Write($"{matrix[i, j], 5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrixRndInt(5, 5, 0, 10);
+PrintArray(matrix);
+Console.WriteLine();
+int[,] matrix2 = SquareBothEvenIndexes(matrix);
+PrintArray(matrix2);
