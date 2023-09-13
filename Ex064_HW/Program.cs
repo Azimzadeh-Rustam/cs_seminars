290d82efb5f41717e76e7c9767fbde24350e2e9d
Rustam <azimzadeh.rustam@gmail.com>
2023-09-13 21:50:17 +0400

    Add Ex064_HW

---
+++ Ex064_HW/Program.cs
@@ -0,0 +1,28 @@
+﻿// 64. Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int ValidationNaturalNumber(int num)
+{
+    while(num < 1)
+    {
+        num = Prompt("Неверно введено число. Введите натуральное число: ");
+    }
+    return num;
+}
+
+void PrintRangeOf(int num)
+{
+    if(num < 1) return;
+    Console.Write($"{num} ");
+    PrintRangeOf(num - 1);
+}
+
+int number = Prompt("Введите целое число: ");
+number = ValidationNaturalNumber(number);
+PrintRangeOf(number);
