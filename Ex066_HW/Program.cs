a7db94c8d5565d8c954173224b48c8db54028509
Rustam <azimzadeh.rustam@gmail.com>
2023-09-16 00:45:59 +0400

    Add Ex066_HW

---
+++ Ex066_HW/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 66. Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int ValidationIfNatural(int num)
+{
+    while(num < 1)
+    {
+        num = Prompt("Неверно введено число. Введите натуральное число: ");
+    }
+    return num;
+}
+
+int SumBetween(int from, int to)
+{
+    if(from == to)
+    {
+        return from;
+    }
+    else
+    {
+        if(from > to)
+        {
+            int temp = from;
+            from = to;
+            to = temp;
+        }
+        checked
+        {
+            return from + SumBetween(from + 1, to);
+        }
+    }    
+}
+
+int numberM = ValidationIfNatural(Prompt("Введите натуральное число M: "));
+int numberN = ValidationIfNatural(Prompt("Введите натуральное число N: "));
+
+int sum = SumBetween(numberM, numberN);
+Console.WriteLine($"Сумма между числами {numberM} и {numberN} равна {sum}");
